77d70df0a433a652a03822bf84130f435e417864
faizunnazif <faizoen75@gmail.com>
2023-12-13 23:34:02 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,99 @@
-import Image from 'next/image'
+import Cards from "@/components/fragments/Cards";
 
 export default function Home() {
+  const data = [
+    {
+      sku: 101,
+      bookName: "Cantik Itu Luka - Eka Kurniawan",
+      link: "https://drive.google.com/file/d/1IAuxhflFbztSXqqpwvnyV9KL7V4OzKel/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/101.jpg",
+    },
+    {
+      sku: 102,
+      bookName: "Cinderella In Paris by Sari Musdar",
+      link: "https://drive.google.com/file/d/1lmnicm0_7yle0EiG7aMbKTgyaScMiWBx/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/102.jpg",
+    },
+    {
+      sku: 103,
+      bookName: "Back Into You by Teresa Bertha",
+      link: "https://drive.google.com/file/d/1Hz_JWC8tOKr01U7Sg84mlo57YFuvtduq/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/103.jpg",
+    },
+    {
+      sku: 104,
+      bookName: "Re dan peREmpuan",
+      link: "https://drive.google.com/file/d/1HEPzCaSwdc2keZS-H0f_KhNySPmX-lPX/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/104.jpg",
+    },
+    {
+      sku: 105,
+      bookName: "Berani Bahagia by Ichiro Kishimi, Fumitake Koga",
+      link: "https://drive.google.com/file/d/1LKuZMMaYEGcDdkPoEHa0Co94o5cuhy4C/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/105.jpg",
+    },
+    {
+      sku: 106,
+      bookName: "Filosofi Teras by Henry Manampiring",
+      link: "https://drive.google.com/file/d/1bmchmLUsxdAvsZ2sqn6Xc12pYHUr_rWe/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/106.jpg",
+    },
+    {
+      sku: 107,
+      bookName:
+        "Funiculi Funicula Before the Coffee Gets Cold by Toshikazu Kawaguchi",
+      link: "https://drive.google.com/file/d/16crDQ35YlPVnRH7Ah1BBFHq2Osyr96Zm/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/107.jpg",
+    },
+    {
+      sku: 108,
+      bookName:
+        "Funiculi Funicula 2 Kisah-Kisah Yang Baru Terungkap by Toshikazu Kawaguchi",
+      link: "https://drive.google.com/file/d/1ecKXcHXX9ser6b6UG1Z0yiKsgihyOyTC/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/108.jpg",
+    },
+    {
+      sku: 109,
+      bookName: "Berani Tidak Disukai by Ichiro Kishimi, Fumitake Koga",
+      link: "https://drive.google.com/file/d/19gkC_RzbPRLWrvXMhuDAPMe3ftRuf9X_/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/109.jpg",
+    },
+    {
+      sku: 110,
+      bookName: "Sebuah Seni untuk Bersikap Bodo Amat by Mark Manson",
+      link: "https://drive.google.com/file/d/1kFx95k7zKYTCk9fnedZ-L9pNtR3z7P7v/view?usp=drive_link",
+      linkPage:
+        "https://raw.githubusercontent.com/Zunnazif/halaman-produk/main/halaman-produk/110.jpg",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="flex flex-wrap justify-center px-1 pt-4">
+        <Cards data={data} />
+        <div className="bg-gray-600 p-3 rounded-full fixed mt-[500px] ml-72">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="16"
+            width="18"
+            viewBox="0 0 576 512"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <path
+              d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"
+              fill="black"
             />
-          </a>
+          </svg>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
---
+++ src/components/fragments/Cards.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+
+export default function Cards({ data }: { data: any }) {
+  return (
+    <>
+      {data.map((item: { linkPage: string; sku: number; bookName: string }) => {
+        return (
+          <div
+            key={item.sku}
+            className="max-w-[160px] bg-slate-950 rounded-md p-2 shadow-md m-1.5"
+          >
+            <span className="bg-yellow-500 px-0.5 absolute ml-0.5">
+              <p className="text-[50%] font-semibold text-red-600">51%</p>
+              <p className="text-[40%] text-white">OFF</p>
+            </span>
+            <Image src={item.linkPage} alt={`page-${item.sku}`} />
+            <div className="content text-white mt-2 px-1">
+              <p className="text-[60%] truncate ">
+                {item.sku}. {item.bookName}
+              </p>
+              <div className="flex my-2">
+                <h3 className="text-red-500 text-xs">
+                  <span className="text-[60%]">Rp</span>980
+                </h3>
+                <p className="text-[50%] ml-2 line-through">Rp2000</p>
+              </div>
+              <div className="flex justify-between">
+                <button className="bg-gray-500 px-5 rounded-xl text-xs">
+                  Beli
+                </button>
+                <div className="flex items-center">
+                  <svg
+                    className="w-2 text-yellow-300 ms-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg
+                    className="w-2 text-yellow-300 ms-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg
+                    className="w-2 text-yellow-300 ms-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg
+                    className="w-2 text-yellow-300 ms-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+
+                  <svg
+                    className="w-2 ms-1 text-gray-300 dark:text-gray-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
